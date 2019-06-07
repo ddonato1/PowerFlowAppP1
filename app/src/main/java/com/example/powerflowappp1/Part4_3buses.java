2f4a43eb357eec1801e1dd80af2c4c0e48ce51fb
gabrielasantiago <gabriela.santiago18@upr.edu>
2019-06-07 15:10:08 -0400

    Fixed part 4 of 3 and 4 buses

--- app/src/main/java/com/example/powerflowappp1/Part4_3buses.java
+++ app/src/main/java/com/example/powerflowappp1/Part4_3buses.java
@@ -3,14 +3,46 @@ package com.example.powerflowappp1;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Part4_3buses extends AppCompatActivity {
+
+    EditText zbus_11;
+    EditText zbus_12;
+    EditText zbus_13;
+    EditText zbus_22;
+    EditText zbus_23;
+    EditText zbus_33;
+
+    private Button nextButtonZ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_part4_3buses);
 
+        zbus_11 = findViewById(R.id.zbus11);
+        zbus_12 = findViewById(R.id.zbus12);
+        zbus_13 = findViewById(R.id.zbus13);
+        zbus_22 = findViewById(R.id.zbus22);
+        zbus_23 = findViewById(R.id.zbus23);
+        zbus_33 = findViewById(R.id.zbus33);
+
+        nextButtonZ = findViewById(R.id.nextbZ);
+        nextButtonZ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nextButtonZ();
+            }
+        });
+
         Intent intent = getIntent();
+    }
+
+    private void nextButtonZ(){
+        Intent intent = new Intent(Part4_3buses.this, Part4_3buses.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/powerflowappp1/Part4_4buses.java
+++ app/src/main/java/com/example/powerflowappp1/Part4_4buses.java
@@ -3,6 +3,7 @@ package com.example.powerflowappp1;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -12,17 +13,13 @@ public class Part4_4buses extends AppCompatActivity {
     EditText zbus_12;
     EditText zbus_13;
     EditText zbus_14;
-    EditText zbus_15;
     EditText zbus_22;
     EditText zbus_23;
     EditText zbus_24;
-    EditText zbus_25;
     EditText zbus_33;
     EditText zbus_34;
-    EditText zbus_35;
     EditText zbus_44;
-    EditText zbus_45;
-    EditText zbus_55;
+
     private Button nextButtonZ;
 
     @Override
@@ -30,6 +27,30 @@ public class Part4_4buses extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_part4_4buses);
 
+        zbus_11 = findViewById(R.id.zbus11);
+        zbus_12 = findViewById(R.id.zbus12);
+        zbus_13 = findViewById(R.id.zbus13);
+        zbus_14 = findViewById(R.id.zbus14);
+        zbus_22 = findViewById(R.id.zbus22);
+        zbus_23 = findViewById(R.id.zbus23);
+        zbus_24 = findViewById(R.id.zbus24);
+        zbus_33 = findViewById(R.id.zbus33);
+        zbus_34 = findViewById(R.id.zbus34);
+        zbus_44 = findViewById(R.id.zbus44);
+
+        nextButtonZ = findViewById(R.id.nextbZ);
+        nextButtonZ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nextButtonZ();
+            }
+        });
+
         Intent intent = getIntent();
+    }
+
+    private void nextButtonZ(){
+        Intent intent = new Intent(Part4_4buses.this, Part4_4buses.class);
+        startActivity(intent);
     }
 }
