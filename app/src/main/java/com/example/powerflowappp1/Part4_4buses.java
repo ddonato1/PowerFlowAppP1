d4bd3c4021cc4a4be580cc4f56b8b6d1b184a979
gabrielasantiago <gabriela.santiago18@upr.edu>
2019-06-07 14:17:13 -0400

    Fixed part 4 of 3 and 4 buses

--- app/src/main/java/com/example/powerflowappp1/Part4_4buses.java
+++ app/src/main/java/com/example/powerflowappp1/Part4_4buses.java
@@ -3,8 +3,27 @@ package com.example.powerflowappp1;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Part4_4buses extends AppCompatActivity {
+
+    EditText zbus_11;
+    EditText zbus_12;
+    EditText zbus_13;
+    EditText zbus_14;
+    EditText zbus_15;
+    EditText zbus_22;
+    EditText zbus_23;
+    EditText zbus_24;
+    EditText zbus_25;
+    EditText zbus_33;
+    EditText zbus_34;
+    EditText zbus_35;
+    EditText zbus_44;
+    EditText zbus_45;
+    EditText zbus_55;
+    private Button nextButtonZ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
